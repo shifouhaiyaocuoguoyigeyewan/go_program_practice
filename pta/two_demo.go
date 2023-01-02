e5387dcb9968400ef83d2158c301c91bc350b0f4
shifouhaiyaocuoguoyigeyewan <2487242076@qq.com>
2023-01-02 23:28:02 +0800

    2023.1.2

--- pta/two_demo.go
+++ pta/two_demo.go
@@ -9,16 +9,16 @@ import "fmt"
 **/
 
 func main() {
-	var n int //n给出的正整数
-	var tag byte //符号
+	var n int            //n给出的正整数
+	var tag byte         //tag符号
+	var sum, nextSum int //sum符号总数量，nextSum下轮符号总数量
 	//_, _ = fmt.Scanf("n:%d, 符号:%c", &n, &tag)
 	n = 21
 	tag = 211
 	var i = 1 //符号行数
-	var sum = 1 
 	//判断符号数量和多余数
 	for {
 		i += 2
-		
+
 	}
 }
