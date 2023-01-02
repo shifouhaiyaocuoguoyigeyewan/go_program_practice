606a403f78c0ed4c01943ec618b5be0a141a32f0
shifouhaiyaocuoguoyigeyewan <2487242076@qq.com>
2023-01-02 23:24:41 +0800

    2023.1.2

--- pta/two.go
+++ pta/two.go
@@ -4,6 +4,8 @@ package main
   @author: Ekko
   @since: 2022/12/30
   @desc: //L1-002 打印沙漏  https://pintia.cn/problem-sets/994805046380707840/exam/problems/994805145370476544
+         解题思路：1.判断符号数量和多余数
+                 2.分上下部分打印符号
 **/
 
 import (
@@ -11,31 +13,31 @@ import (
 )
 
 func  main() {
-	var n int
-	var tag byte
-	var sum, nextSum int
-	_, _ = fmt.Scanf("%d %c", &n, &tag)
-	//n = 19
-	//tag = 255
+	var n int //n给出的正整数
+	var tag byte //tag符号，byte类型，相当于uint8(0-255)
+	var sum, nextSum int //sum需要符号数量，nextSum下轮符号总数量
+	//_, _ = fmt.Scanf("%d %c", &n, &tag)
+	n = 19
+	tag = 255
 
 	sum = 1
 	nextSum = 1
-	i := 1
+	i := 1 //i符号行数
 	last := 0
-	for true { // 先是计算出所需要多少的符号
+	for { // 先是计算出所需要多少的符号
 		i += 2
 		nextSum += i * 2
 		if nextSum <= n {
 			sum += i * 2
 		} else {
-			i -= 2
-			last = n - sum
-			break
+  			i -= 2
+			last = n - sum //多出数
+   			break
 		}
 	}
 
 	for j:=i; j>=1; j=j-2 {  // 先打印上半部分
-		var space = (i - j) / 2
+		var space = (i - j) / 2 //space代表打印的空白
 		for k:=0; k<i-space; k++ {
 			if k < space {
 				fmt.Printf(" ")  // 先打印空格
---
+++ pta/two_demo.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+/**
+  @author: Ekko
+  @since: 2023/1/2
+  @desc: //TODO
+**/
+
+func main() {
+	var n int //n给出的正整数
+	var tag byte //符号
+	//_, _ = fmt.Scanf("n:%d, 符号:%c", &n, &tag)
+	n = 21
+	tag = 211
+	var i = 1 //符号行数
+	var sum = 1 
+	//判断符号数量和多余数
+	for {
+		i += 2
+		
+	}
+}
